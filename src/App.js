3fd0d5232ca7edbd6d4ffd3770d0d4eddf64c207
Henrycodeproj <87413740+Henrycodeproj@users.noreply.github.com>
2022-02-10 00:58:49 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SaveRecipe from './components/SaveList/RecipeHandler'
 import {RecipeContext} from './Context/context'
 
 function App() {
-
+// functions in context
   return (
     <div className="App">
       <RecipeContext>
