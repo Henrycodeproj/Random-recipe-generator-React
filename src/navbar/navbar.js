6581e3258313f75a2e049cc0120d9cfdaf417f03
Henry <lihenryhl@yahoo.com>
2022-02-17 03:02:05 -0800

    finally got the search bar to work with active links to the queried recipes

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 
     const [search, setSearch] = useState('')
 
-    const {items} = useContext(recipeItems)
+    const {setItems} = useContext(recipeItems)
 
     const searchBar = (e) => {
         const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
@@ -14,23 +14,36 @@ export const Navbar = () => {
             const response = await fetch(`${URL}${e.target.value}`)
             const results = await response.json()
             const meals = await results.meals
-            meals.map((meal)=> `<a>${meal.strMeal}</a>`)
+            //console.log(meals)
             setSearch(meals)
         }
 
         (async () => fetchData())()
 
     }
-
+    const handlesearchLinks = (meal) => {
+        setItems(meal)
+    }  
     return (
         <nav>
             <div>test</div>
             <ul>
-                <input type = "text" placeholder="Type a category or meal" onChange={(e)=> searchBar(e)}></input>
-                {/* {search ? search.map((meal) => `<a>${meal.strMeal}</a>`): console.log('nothing')} */}
                 <li>test2</li>
                 <li>test3</li>
             </ul>
+            <input type = "text" placeholder="Type a category or meal" onChange={(e)=> searchBar(e)}></input>
+                    <div className="searchContainer">
+                        { //begining of conditional
+                        search ? search.map((meal) => // first condition checks to see if there is any information typed in search bar
+                        <ul>
+                            <li onClick={() => handlesearchLinks(meal)}> 
+                                {meal.strMeal}
+                            </li>
+                        </ul>
+                        ):// If there is nothing in search state, it will render nothing.
+                        console.log('nothing')
+                        }
+                    </div>
         </nav>
     )
 }
